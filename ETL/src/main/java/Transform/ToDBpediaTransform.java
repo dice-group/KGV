0dbbd896a829fbfb74b77032989ffb62054d344c
farshad68 <farshad.afshari68@gmail.com>
2021-03-18 17:05:26 +0100

    ttl file to model transform , SimpleRDF transform with random ID

---
+++ ETL/src/main/java/Transform/ToDBpediaTransform.java
@@ -0,0 +1,12 @@
+package Transform;
+
+import Model.SimpleRDF;
+
+import java.util.List;
+
+public class ToDBpediaTransform implements ITransform<List<SimpleRDF>,List<SimpleRDF>>{
+    @Override
+    public List<SimpleRDF> Transform(List<SimpleRDF> input, String splitter) throws Exception {
+        return null;
+    }
+}
---
+++ QueryRunner/src/test/java/QueryRunner/SimpleSparqlRunnerTest.java
@@ -0,0 +1,134 @@
+package QueryRunner;
+
+import org.apache.jena.query.ParameterizedSparqlString;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSparqlRunnerTest {
+    @Test
+    public void SimpleTestShouldRun(){
+        SimpleSparqlRunner queryExecutioner = new SimpleSparqlRunner("http://localhost:3030/myGraph/query");
+
+
+        ParameterizedSparqlString paraPathQuery =
+                new ParameterizedSparqlString("SELECT ?s ?p ?o\n" +
+                        "WHERE { ?s ?p ?o }\n" +
+                        "LIMIT 100");
+
+        try(QueryExecution qe = queryExecutioner.getQueryExecution(paraPathQuery.asQuery());){
+            ResultSet result = qe.execSelect();
+
+            StringBuilder sb = new StringBuilder();
+
+            while (result.hasNext()) {
+                QuerySolution qs = result.next();
+
+                sb.append(" subject is : "+qs.get("?s"));
+                sb.append(" predicate is : "+qs.get("?p"));
+                sb.append(" object is : "+qs.get("?o"));
+                sb.append(" |||||||||| ");
+            }
+
+            System.out.println(sb.toString());
+        }catch (Exception ex){
+            throw ex;
+        }
+    }
+
+
+    @Test
+    public void SelectAllPredicateAndObjectForASubject(){
+        SimpleSparqlRunner queryExecutioner = new SimpleSparqlRunner("http://localhost:3030/myGraph/query");
+
+
+        ParameterizedSparqlString paraPathQuery =
+                new ParameterizedSparqlString("SELECT ?p ?o\n" +
+                        "WHERE { <http://yago-knowledge.org/resource/Bill_Gates> ?p ?o }\n" +
+                        "LIMIT 100");
+
+        try(QueryExecution qe = queryExecutioner.getQueryExecution(paraPathQuery.asQuery());){
+            ResultSet result = qe.execSelect();
+
+            StringBuilder sb = new StringBuilder();
+
+            while (result.hasNext()) {
+                QuerySolution qs = result.next();
+
+                sb.append(" predicate is : "+qs.get("?p"));
+                sb.append(" object is : "+qs.get("?o"));
+                sb.append(" |||||||||| ");
+            }
+
+            System.out.println(sb.toString());
+        }catch (Exception ex){
+            throw ex;
+        }
+    }
+
+
+    @Test
+    public void CountSubjectAndObjects(){
+        SimpleSparqlRunner queryExecutioner = new SimpleSparqlRunner("http://localhost:3030/myGraph/query");
+
+
+        ParameterizedSparqlString paraPathQuery =
+                new ParameterizedSparqlString("SELECT  (count(*) AS ?c)"+
+        "WHERE"+
+        "{ ?s  <http://yago-knowledge.org/resource/livesIn>  ?o}");
+
+        try(QueryExecution qe = queryExecutioner.getQueryExecution(paraPathQuery.asQuery());){
+            ResultSet result = qe.execSelect();
+
+            StringBuilder sb = new StringBuilder();
+
+            while (result.hasNext()) {
+                QuerySolution qs = result.next();
+
+                sb.append(" c is : "+qs.get("?c"));
+                sb.append(" |||||||||| ");
+            }
+
+            System.out.println(sb.toString());
+        }catch (Exception ex){
+            throw ex;
+        }
+    }
+
+
+
+    @Test
+    public void AllPredicates(){
+        SimpleSparqlRunner queryExecutioner = new SimpleSparqlRunner("http://localhost:3030/myGraph/query");
+
+
+        ParameterizedSparqlString paraPathQuery =
+                new ParameterizedSparqlString("SELECT  DISTINCT(?p)\n" +
+                        "WHERE { ?s ?p ?o }\n" +
+                        "LIMIT 100");
+
+        try(QueryExecution qe = queryExecutioner.getQueryExecution(paraPathQuery.asQuery());){
+            ResultSet result = qe.execSelect();
+
+            StringBuilder sb = new StringBuilder();
+
+            while (result.hasNext()) {
+                QuerySolution qs = result.next();
+
+                sb.append(" predicate is : "+qs.get("?p"));
+                sb.append(" |||||||||| ");
+            }
+
+            System.out.println(sb.toString());
+        }catch (Exception ex){
+            throw ex;
+        }
+    }
+
+}
